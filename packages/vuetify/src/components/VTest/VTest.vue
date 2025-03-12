d8745e01ea9b5c810ec723bd6203703759a7dacb
蓝色 <40845795+shixiulong@users.noreply.github.com>
2025-03-12 16:02:43 +0800

    init

---
+++ packages/vuetify/src/components/VTest/VTest.vue
@@ -0,0 +1,13 @@
+<template>
+  count: {{ count }}
+  <v-btn @click="add" active/>
+</template>
+
+<script setup>
+  // Utilities
+  import { ref } from 'vue'
+  const count = ref(0)
+  const add = () => {
+    count.value++
+  }
+</script>
