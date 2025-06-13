7610b7855623722c455d72cf16a79734f5a0b4bc
Hank <17625020323@163.com>
2025-06-13 14:28:44 +0800

    TTMP-16241:筛选组件完成

---
+++ packages/hc-component/dev/pages/components/hc-filter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <HcFilter>
+            <template #filter-input>
+                <div>
+                    <v-text-field label="111" v-model="a"></v-text-field>
+                </div>
+                <div>
+                    <v-text-field label="111" v-model="a"></v-text-field>
+                </div>
+                <div>
+                    <v-text-field label="111" v-model="a"></v-text-field>
+                </div>
+            </template>
+        </HcFilter>
+    </div>
+  </template>
+  
+  <script setup>
+    import { ref } from 'vue'
+    import HcFilter from '../../../src/components/custom/HcFilter/hcfilter.vue'
+    const a = ref('111')
+  </script>
+  
+  <style scoped lang="scss">
+  </style>
+  
