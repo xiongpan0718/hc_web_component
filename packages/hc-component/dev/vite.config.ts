13864b5a46050e0ced9f87ebd86a9147fb45b43e
xiongpan0718 <xiongpan0718@hotmail.com>
2026-03-20 13:26:41 +0800

    fix deployment config

--- packages/hc-component/dev/vite.config.ts
+++ packages/hc-component/dev/vite.config.ts
@@ -4,6 +4,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: process.env.BASE_PATH || '/',
   css: {
     preprocessorOptions: {
       scss: { silenceDeprecations: ['legacy-js-api'] },
